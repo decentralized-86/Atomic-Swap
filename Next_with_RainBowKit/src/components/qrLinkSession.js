6190f0799de33c72be4db6afd686739a214395e2
Devign Experience <devignx@gmail.com>
2023-11-16 21:02:30 +0530

    ui updates

--- Next_with_RainBowKit/src/components/qrLinkSession.js
+++ Next_with_RainBowKit/src/components/qrLinkSession.js
@@ -1,59 +1,64 @@
-import { useState , useEffect} from 'react';
-import { QRCodeSVG } from 'qrcode.react';
+import { useState, useEffect } from "react";
+import { QRCodeSVG } from "qrcode.react";
 
 function SwapSession() {
-  const [sessionId, setSessionId] = useState(''); 
-  const baseURL = 'https://AtomicSwap.com/swap';
+    const [sessionId, setSessionId] = useState("");
+    const baseURL = "https://AtomicSwap.com/swap";
 
-  const generateSessionId = () => {
-    console.log("Entered session id")
-    const uniqueId = "22432424";
-    setSessionId(uniqueId);
-    console.log("Session Id is been set okay ")
-  };
+    const generateSessionId = () => {
+        console.log("Entered session id");
+        const uniqueId = "22432424";
+        setSessionId(uniqueId);
+        console.log("Session Id is been set okay ");
+    };
 
-  const handleCopyClick = () => {
-    if (sessionId) {
-      navigator.clipboard.writeText(sessionURL);
-      alert('Session URL copied to clipboard!');
-    }
-  };
-  
-useEffect(()=>{
-  console.log("Session Id",sessionId);
-},[sessionId])
+    const handleCopyClick = () => {
+        if (sessionId) {
+            navigator.clipboard.writeText(sessionURL);
+            alert("Session URL copied to clipboard!");
+        }
+    };
 
+    useEffect(() => {
+        console.log("Session Id", sessionId);
+    }, [sessionId]);
 
-  const sessionURL = sessionId ? `${baseURL}?session_id=${sessionId}` : '';
+    const sessionURL = sessionId ? `${baseURL}?session_id=${sessionId}` : "";
 
-  return (
-    <div className="flex flex-col items-center space-y-4">
-      <button 
-        className="bg-blue-500 text-white px-4 py-2 rounded" 
-        onClick={generateSessionId}
-      >
-        Create Swap Session
-      </button>
-      {sessionId && (
-        <>
-          <QRCodeSVG value={sessionURL} />
-          <p>
-            Session URL:{' '}
-            <a href={sessionURL} target="_blank" rel="noopener noreferrer">
-              {sessionURL}
-            </a>{' '}
-            <span
-              role="button"
-              className="cursor-pointer text-blue-500"
-              onClick={handleCopyClick}
+    return (
+        <div className="flex flex-col items-center space-y-4">
+            <button
+                disabled={sessionId}
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+                onClick={generateSessionId}
             >
-              (Copy)
-            </span>
-          </p>
-        </>
-      )}
-    </div>
-  );
+                Create Swap Session
+            </button>
+
+            {sessionId && (
+                <>
+                    <QRCodeSVG value={sessionURL} />
+                    <p>
+                        Session URL:{" "}
+                        <a
+                            href={sessionURL}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {sessionURL}
+                        </a>{" "}
+                        <span
+                            role="button"
+                            className="cursor-pointer text-blue-500"
+                            onClick={handleCopyClick}
+                        >
+                            (Copy)
+                        </span>
+                    </p>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default SwapSession;
--- Next_with_RainBowKit/src/pages/index.js
+++ Next_with_RainBowKit/src/pages/index.js
@@ -1,23 +1,101 @@
-import { Inter } from 'next/font/google'
-import { ConnectButton } from '@rainbow-me/rainbowkit'
-import {useAccount} from "wagmi"
-import SwapSession from '@/components/qrLinkSession'
+import { Inter } from "next/font/google";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
+import SwapSession from "@/components/qrLinkSession";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const { isConnected } = useAccount();
-  return (
-    <div>
-    <main
-      className={`flex h-screen items-center justify-center ${inter.className}`}
-    >
-      <div className="flex flex-col items-center space-y-4">
-        <h2 className="text-2xl font-bold">Atomic Swap</h2>
-        <ConnectButton />
-        {isConnected && <SwapSession/>}
-      </div>
-    </main>
-    </div>
-  )
+    const dummyData = [
+        {
+            name: "Name",
+            amount: 1000,
+        },
+        {
+            name: "Name",
+            amount: 1000,
+        },
+    ];
+    const { isConnected } = useAccount();
+    return (
+        <div>
+            <main
+                className={`flex h-screen items-center justify-center ${inter.className}`}
+            >
+                <div className=" fixed left-1/2 text-center -translate-x-1/2 top-8 mb-6 font-bold">
+                    <span className="my-6 text-2xl block">Atomic Swap</span>
+                    <ConnectButton />
+                </div>
+                <div className="flex gap-12 items-center space-y-4">
+                    <div id="send" className="flex flex-col gap-2 w-[15rem]">
+                        <h2 className="text-2xl text-center mb-6 font-bold">
+                            You'll send
+                        </h2>
+                        {dummyData.length > 0 ? (
+                            <div className="flex flex-col gap-2">
+                                {dummyData.map((item, index) => (
+                                    <div
+                                        key={index}
+                                        className="bg-slate-800 flex justify-between gap-2 p-3 rounded-lg px-4"
+                                    >
+                                        <div className="flex gap-1">
+                                            <p>✔</p>
+                                            <p>{item.name}</p>
+                                        </div>
+                                        <div className="flex gap-3">
+                                            <p className="bg-blue-500 rounded-md p-1 px-2 text-xs">
+                                                {item.amount}
+                                            </p>
+                                            <button className="text-red-500">
+                                                x
+                                            </button>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        ) : (
+                            <p>Nothing to show here</p>
+                        )}
+                        <button className="bg-blue-500 px-5 p-2 mt-6 rounded-lg">
+                            Add asset
+                        </button>
+                    </div>
+
+                    <div className="text-center max-w-[400px] flex flex-col gap-8 justify-center items-center">
+                        {isConnected && <SwapSession />}
+                    </div>
+
+                    <div id="send" className="flex flex-col gap-2 w-[15rem]">
+                        <h2 className="text-2xl text-center mb-6 font-bold">
+                            You'll Recieve
+                        </h2>
+                        <div className="flex flex-col gap-2">
+                            {dummyData.map((item, index) => (
+                                <div
+                                    key={index}
+                                    className="bg-slate-800 flex justify-between gap-2 p-3 rounded-lg px-4"
+                                >
+                                    <div className="flex gap-1">
+                                        <p>✔</p>
+                                        <p>{item.name}</p>
+                                    </div>
+                                    <div className="flex gap-3">
+                                        <p className="bg-blue-500 rounded-md p-1 px-2 text-xs">
+                                            {item.amount}
+                                        </p>
+                                        <button className="text-red-500">
+                                            x
+                                        </button>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                        <button className="bg-blue-500 px-5 p-2 mt-6 rounded-lg">
+                            Add asset
+                        </button>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
 }
